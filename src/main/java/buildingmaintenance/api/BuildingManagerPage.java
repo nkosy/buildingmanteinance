ba6d9e0961e3c04f106079f1cf38b9c885197f98
Your Name <you@example.com>
2015-10-02 16:49:14 +0200

    Removed Hateos Been having problems there

--- src/main/java/buildingmaintenance/api/BuildingManagerPage.java
+++ src/main/java/buildingmaintenance/api/BuildingManagerPage.java
@@ -28,77 +28,27 @@ public class BuildingManagerPage {
     private BuildingManagerService service;
 
     @RequestMapping(value = "/managers", method = RequestMethod.GET)
-    public List<BuildingManagerResource> getManagers(){
-
-        List<BuildingManagerResource> hateos = new ArrayList<>();
-        List<BuildingManager> managers = service.findAll();
-
-        for (BuildingManager manager : managers) {
-            BuildingManagerResource res = new BuildingManagerResource
-                    .Builder(manager.getManager_name())
-                    .resID(manager.getManager_id())
-                    .buildings(manager.getBuildings())
-                    .build();
-            Link buildings = new
-                    Link("http://buildingmanteinance-nkosi.rhcloud.com/api/manager/"+res.getResID())
-                    .withRel("buildings");
-
-            res.add(buildings);
-            hateos.add(res);
-        }
-        return hateos;
+    public List<BuildingManager> getManagers(){
+        return service.findAll();
     }
 
     @RequestMapping(value = "/managerids", method = RequestMethod.GET)
-    public List<BuildingManagerResource>  getManagerByID(
+    public BuildingManager  getManagerByID(
             @RequestParam(value =  "id", defaultValue = "1") Long id){
 
-        BuildingManager manager =  service.findById(id);
-        List<BuildingManagerResource> hateos = new ArrayList<>();
-
-        BuildingManagerResource res = new BuildingManagerResource
-                .Builder(manager.getManager_name())
-                .resID(manager.getManager_id())
-                .buildings(manager.getBuildings())
-                .build();
-        Link buildings = new
-                Link("http://buildingmanteinance-nkosi.rhcloud.com/api/manager/"+res.getResID())
-                .withRel("buildings");
-
-        res.add(buildings);
-        hateos.add(res);
-        return  hateos;
+        return service.findById(id);
     }
 
     @RequestMapping(value = "/managernames", method = RequestMethod.GET)
-    public List<BuildingManagerResource> getManagerByName(
+    public BuildingManager getManagerByName(
             @RequestParam(value =  "name", defaultValue = "lil Wayne") String name){
 
-        BuildingManager manager =  service.findByName(name);
-        List<BuildingManagerResource> hateos = new ArrayList<>();
-
-        BuildingManagerResource res = new BuildingManagerResource
-                .Builder(manager.getManager_name())
-                .resID(manager.getManager_id())
-                .buildings(manager.getBuildings())
-                .build();
-        Link buildings = new
-                Link("http://buildingmanteinance-nkosi.rhcloud.com/api/manager/"+res.getResID())
-                .withRel("buildings");
-        res.add(buildings);
-        hateos.add(res);
-        return  hateos;
+       return service.findByName(name);
     }
 
     @RequestMapping(value = "/buildingmanager/create", method = RequestMethod.POST)
     public ResponseEntity<Void> createManager(@RequestBody BuildingManager manager, UriComponentsBuilder ucBuilder) {
         System.out.println("Creating Manager " + manager.getManager_name());
-
-//     USE THIS IF YOU WANT TO CHECK UNIQUE OBJECT
-//      if (SubjectService.isSubjectExist(Subject)) {
-//            System.out.println("A Subject with name " + Subject.getName() + " already exist");
-//            return new ResponseEntity<Void>(HttpStatus.CONFLICT);
-//        }
 
         service.save(manager);
 
